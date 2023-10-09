65ea2c3aa24dfd20d46724751bc737b573c798d2
Edwin van Meerendonk <vanmeerendonk@gmail.com>
2023-10-09 21:28:23 +0200

    Add files via upload

---
+++ fileHandler.js
@@ -0,0 +1,33 @@
+const fs = require('fs').promises;
+
+/**
+ * Writes the provided security list to a file, formatted as JSON.
+ * @param {object} sl - The security list to write.
+ * @param {string} vcnName - The name of the VCN.
+ */
+async function writeSecurityListToFile(sl, vcnName) {
+    let fileName = vcnName + "_" + sl.displayName + ".json";
+    fileName = fileName.replace(/[/.]/g, '_');
+    fileName = `./output/${fileName}`;
+    try {
+        await fs.writeFile(fileName, JSON.stringify(sl, null, 2)); // 2 spaces for formatting
+    } catch (error) {
+        console.error(`Error writing to file ${fileName}:`, error.message);
+    }
+}
+
+/**
+ * Deletes all files within the provided directory path.
+ * @param {string} directoryPath - The path to the directory.
+ */
+async function deleteFilesInDirectory(directoryPath) {
+    try {
+        const files = await fs.readdir(directoryPath);
+        const deletePromises = files.map(file => fs.unlink(`${directoryPath}/${file}`));
+        await Promise.all(deletePromises);
+    } catch (error) {
+        console.error(`Error deleting files in ${directoryPath}:`, error);
+    }
+}
+
+module.exports = { deleteFilesInDirectory, writeSecurityListToFile };
